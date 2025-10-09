d7ee6f413769ce6929b6aa623a65274fdc7393bb
JakubBron <jakub.bronowski1@gmail.com>
2025-10-09 17:11:07 +0200

    Lab1 pt. 1 (20%)

---
+++ lab01/1-UTNode/src/util.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+	//this.Hello = function(name) { return "Helloooo to " + name; }
+	// fixed: typo
+	this.Hello = function(name) { return "Hello to " + name; }
+}
+
---
+++ lab01/2-UTNodeInBrowser/src/worker.js
@@ -0,0 +1,7 @@
+function Worker(name) {
+	//this.Introduce = function() { return "Helloooo I'm " + name; }
+	// fixed: typo
+	this.Introduce = function() { return "Hello I'm " + name; }
+		this.DoJob = function(task) { return "Worker " + name + " does " + task; }
+} 
+
